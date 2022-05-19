c19e2c681ce3dc98ecf20938e2ad6ff49a546393
Ryan <ryaaan_@live.co.uk>
2022-05-20 00:40:32 +0100

    Progress with form using react-form-hook and tweaked ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
-import PostBox from '../components/postbox'
+import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="my-7 mx-auto max-w-5xl ">
       <Head>
         <title>Ryan's Reddit Clone</title>
       </Head>
